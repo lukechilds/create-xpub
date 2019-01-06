efaba9d80c93b47a590c6481ecb785ce4879d708
Luke Childs <lukechilds123@gmail.com>
2019-01-06 07:37:01 +0700

    Test compressed or uncompressed public key

--- test/unit.js
+++ test/unit.js
@@ -12,6 +12,10 @@ const tpubTestParams = {
 	...xpubTestParams,
 	network: TPUB
 };
+const xpubCompressedPublicKeyTestParams = {
+	...xpubTestParams,
+	publicKey: '038bcdcf59f046b13f1eb35b608d1211265fde8cc44fc7a5a7f7107c5cf2380953'
+};
 const expectedXpub = 'xpub6CgMcBZk66ayM9ESh7QtBmRKJbsa6rBeBH2k4aQZQJGossryP5r2N2nQS4hBMG1wb8igPoH53bxtzTBaeMqJkbu8bxsih1gGkoAn23Nr8VP';
 const expectedTpub = 'tpubDD3z8RPRoNYRcwRJ9JPyPgkgdiyE6Eghiud3R8ThkD2hdAXgTJh7WUTEg6mxskyBP3Fb1NnwahnwgdgC3DgYe3MRfZd2NYLWLkmBn7UWZXk';
 
@@ -27,4 +31,10 @@ test('createXpub is serialised correctly', t => {
 test('Different networks can be passed in', t => {
 	const tpub = createXpub(tpubTestParams);
 	t.is(tpub, expectedTpub);
+});
+
+test('Handles compressed or uncompressed public key', t => {
+	const xpubUncompressedKey = createXpub(xpubTestParams);
+	const xpubCompressedKey = createXpub(xpubCompressedPublicKeyTestParams);
+	t.is(xpubUncompressedKey, xpubCompressedKey);
 });
