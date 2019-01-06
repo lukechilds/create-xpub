da71b988c50596662fa9539064027a27da74f523
Luke Childs <lukechilds123@gmail.com>
2019-01-06 07:29:25 +0700

    Test different networks can be passed in

--- test/unit.js
+++ test/unit.js
@@ -1,19 +1,30 @@
 import test from 'ava';
 import createXpub from '..';
 
-const testParams = {
+const TPUB = 0x043587CF;
+const xpubTestParams = {
 	depth: 3,
 	childnum: 2147483648,
 	chainCode: '84cf7d9029cdd9fcadbb3717fd92ec0db7d7d9787c57c13c08fc887c389b566b',
 	publicKey: '048bcdcf59f046b13f1eb35b608d1211265fde8cc44fc7a5a7f7107c5cf238095328a0e0d7be17c7d3e48490e8c6433af6d2c3dacc687f3fecaa98a3d05f17de97'
 };
+const tpubTestParams = {
+	...xpubTestParams,
+	network: TPUB
+};
 const expectedXpub = 'xpub6CgMcBZk66ayM9ESh7QtBmRKJbsa6rBeBH2k4aQZQJGossryP5r2N2nQS4hBMG1wb8igPoH53bxtzTBaeMqJkbu8bxsih1gGkoAn23Nr8VP';
+const expectedTpub = 'tpubDD3z8RPRoNYRcwRJ9JPyPgkgdiyE6Eghiud3R8ThkD2hdAXgTJh7WUTEg6mxskyBP3Fb1NnwahnwgdgC3DgYe3MRfZd2NYLWLkmBn7UWZXk';
 
 test('createXpub is exported', t => {
 	t.not(createXpub, undefined);
 });
 
 test('createXpub is serialised correctly', t => {
-	const xpub = createXpub(testParams);
+	const xpub = createXpub(xpubTestParams);
 	t.is(xpub, expectedXpub);
+});
+
+test('Different networks can be passed in', t => {
+	const tpub = createXpub(tpubTestParams);
+	t.is(tpub, expectedTpub);
 });
