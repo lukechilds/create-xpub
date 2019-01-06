6853c4452edb8ffafd5d49c00a7869be6d3ba51e
Luke Childs <lukechilds123@gmail.com>
2019-01-06 07:38:59 +0700

    Make tests more readable

--- test/unit.js
+++ test/unit.js
@@ -8,14 +8,6 @@ const xpubTestParams = {
 	chainCode: '84cf7d9029cdd9fcadbb3717fd92ec0db7d7d9787c57c13c08fc887c389b566b',
 	publicKey: '048bcdcf59f046b13f1eb35b608d1211265fde8cc44fc7a5a7f7107c5cf238095328a0e0d7be17c7d3e48490e8c6433af6d2c3dacc687f3fecaa98a3d05f17de97'
 };
-const tpubTestParams = {
-	...xpubTestParams,
-	network: TPUB
-};
-const xpubCompressedPublicKeyTestParams = {
-	...xpubTestParams,
-	publicKey: '038bcdcf59f046b13f1eb35b608d1211265fde8cc44fc7a5a7f7107c5cf2380953'
-};
 const expectedXpub = 'xpub6CgMcBZk66ayM9ESh7QtBmRKJbsa6rBeBH2k4aQZQJGossryP5r2N2nQS4hBMG1wb8igPoH53bxtzTBaeMqJkbu8bxsih1gGkoAn23Nr8VP';
 const expectedTpub = 'tpubDD3z8RPRoNYRcwRJ9JPyPgkgdiyE6Eghiud3R8ThkD2hdAXgTJh7WUTEg6mxskyBP3Fb1NnwahnwgdgC3DgYe3MRfZd2NYLWLkmBn7UWZXk';
 
@@ -29,12 +21,18 @@ test('createXpub is serialised correctly', t => {
 });
 
 test('Different networks can be passed in', t => {
-	const tpub = createXpub(tpubTestParams);
+	const tpub = createXpub({
+		...xpubTestParams,
+		network: TPUB
+	});
 	t.is(tpub, expectedTpub);
 });
 
 test('Handles compressed or uncompressed public key', t => {
 	const xpubUncompressedKey = createXpub(xpubTestParams);
-	const xpubCompressedKey = createXpub(xpubCompressedPublicKeyTestParams);
+	const xpubCompressedKey = createXpub({
+		...xpubTestParams,
+		publicKey: '038bcdcf59f046b13f1eb35b608d1211265fde8cc44fc7a5a7f7107c5cf2380953'
+	});
 	t.is(xpubUncompressedKey, xpubCompressedKey);
 });
